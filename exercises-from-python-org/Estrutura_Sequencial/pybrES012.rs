9f82b5aa8583d9a2f4eee39bc9ab84d69519f195
kswelder <welderhayne2000@gmail.com>
2021-07-28 19:51:49 -0300

    Calculo IMC

--- exercises-from-python-org/Estrutura_Sequencial/pybrES012.rs
+++ exercises-from-python-org/Estrutura_Sequencial/pybrES012.rs
@@ -5,11 +5,17 @@ fn main() {
     // Tendo como dados de entrada a altura de uma pessoa,
 	// construa um algoritmo que calcule seu peso ideal,
 	// usando a seguinte fórmula: (72.7*altura) - 58 
+    
+    println!("Digite sua altura");
+    let altura: f32 = scan_float();
+    println!("Digite seu peso");
+    let peso: f32 = scan_float();
+    println!("Seu IMC eh {}", peso / (altura * altura));
 }
 
-fn scan_int()-> i32 {
+fn scan_float()-> f32 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let number: i32 = input.trim().parse().unwrap();
+    let number: f32 = input.trim().parse().unwrap();
     return number;
 }
