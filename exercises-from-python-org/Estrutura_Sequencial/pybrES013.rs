b565fd134c7a098bc86a77054c473eba3b2d22ca
kswelder <welderhayne2000@gmail.com>
2021-07-28 19:52:47 -0300

    IMC para homens e mulheres

--- exercises-from-python-org/Estrutura_Sequencial/pybrES013.rs
+++ exercises-from-python-org/Estrutura_Sequencial/pybrES013.rs
@@ -2,12 +2,21 @@ use std::io;
 
 fn main() {
     // https://wiki.python.org.br/EstruturaSequencial
-    // 
+    // Tendo como dado de entrada a altura (h) de uma pessoa,
+    // construa um algoritmo que calcule seu peso ideal,
+    // utilizando as seguintes fórmulas:
+
+    // Para homens: (72.7*h) - 58
+    // Para mulheres: (62.1*h) - 44.7
+
+    println!("Digite sua altura");
+    let h = scan();
+    println!("Homen: {}\nMulher: {}", (72.7 * h) - 58.0, (62.1 * h) - 44.7);
 }
 
-fn scan_int()-> i32 {
+fn scan()-> f32 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let number: i32 = input.trim().parse().unwrap();
+    let number: f32 = input.trim().parse().unwrap();
     return number;
 }
