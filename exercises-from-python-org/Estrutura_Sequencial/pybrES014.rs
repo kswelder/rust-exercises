5fdc0c1c46d8e28266a31e77fdf2f1fd3d278e1e
kswelder <welderhayne2000@gmail.com>
2021-07-28 19:53:32 -0300

    Calculo de multa por kg peixes exedentes

--- exercises-from-python-org/Estrutura_Sequencial/pybrES014.rs
+++ exercises-from-python-org/Estrutura_Sequencial/pybrES014.rs
@@ -2,12 +2,36 @@ use std::io;
 
 fn main() {
     // https://wiki.python.org.br/EstruturaSequencial
-    // 
+    // João Papo-de-Pescador, homem de bem,
+    // comprou um microcomputador para controlar o
+    // rendimento diário de seu trabalho.
+    // Toda vez que ele traz um peso de peixes maior
+    // que o estabelecido pelo regulamento de pesca
+    // do estado de São Paulo (50 quilos) deve pagar
+    // uma multa de R$ 4,00 por quilo excedente.
+    // João precisa que você faça um programa que leia
+    // a variável peso (peso de peixes) e calcule o excesso.
+    // Gravar na variável excesso a quantidade de quilos além
+    // do limite e na variável multa o valor da multa que
+    // João deverá pagar.
+    // Imprima os dados do programa com as mensagens adequadas.
+
+    let exedente: f32 = 50.0;
+    let multa: f32 = 4.0;
+
+    println!("Digite quantos KG de peixes voce pegou.");
+    let peixes = scan_int();
+    if exedente < peixes {
+        println!("Voce pegou {}KG peixes a mais do limite.\nSua multa eh de R${}", peixes - exedente, (peixes - exedente) * multa);
+    }
+    else {
+        println!("Voce pegou {}KG peixes, entao nao precisa pagar multa.", peixes);
+    }
 }
 
-fn scan_int()-> i32 {
+fn scan_int()-> f32 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let number: i32 = input.trim().parse().unwrap();
+    let number: f32 = input.trim().parse().unwrap();
     return number;
 }
